05e5324d3255f2804d52d193843018fe51e7bbd8
Cristiana David <cristiana.radu92@yahoo.com>
2025-04-06 01:33:16 +0100

    Create global variables for the hame

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import tkinter
+import random
+
+
+# Global variables
+current_player = "X"
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
+turns = 0
+winner = None
+game_running = True
+
+
+# colors to assign to window game
+color_green = "#00ff00"
+color_red = "#ff0000"
+color_gray = "#343434"
+color_light_gray = "#cccccc"
+
+
+""" 
+Code inspired from https://www.youtube.com/watch?v=nbRpDXV7QDM
+to create window for tic tac toe game. 
+More info in Readme
+"""
+
+
