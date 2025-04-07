84ba2793109183d83ebb6ec678fcf1da4d7f5c9b
Cristiana David <cristiana.radu92@yahoo.com>
2025-04-07 20:30:06 +0100

    Add instructions to the game

--- run.py
+++ run.py
@@ -10,6 +10,27 @@ winner = None
 game_running = True
 
 
+def instructions():
+    """
+    Prints instructions for the game.
+    """
+    print("*" * 5 + "Welcome to TIC TAC TOE game" + "*" * 5 + "\n")
+    print(">>>>>>>>INSTRUCTIONS<<<<<<<<\n")
+    print("Tic-tac-toe is a game played on a three-by-three grid")
+    print("by two players, in this case you and the computer, who alternately")
+    print("place the marks X and O in one of the nine spaces in the grid.\n")
+    print("Consider a board with the nine positions numbered as follows:\n")
+    print(" 0 | 1 | 2 |")
+    print("---|---|---|")
+    print(" 3 | 4 | 5 |")
+    print("---|---|---|")
+    print(" 6 | 7 | 8 |\n")
+    print("Choose a position from 0 to 8 in order to mark.")
+    print("Whoever gets the first 3 symbols in a row, column or diagonal,")
+    print("is the winner of the game!\n")
+    print(">>>GOOD LUCK!<<<")
+
+
 def print_game_board(game_board):
     pass 
 
@@ -55,8 +76,17 @@ def exit_game(game_window):
     pass
             
 
-while game_running:
+def main():
+    """
+    Runs all the program functions
+    """
+    instructions()
+    print_game_board(game_board)
     mark_choice(game_board)
     switch_player()
     computer(game_board)
 
+
+if __name__ == '__main__':
+    while True:
+        main()
