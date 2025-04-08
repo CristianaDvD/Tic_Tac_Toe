1be070e4a1206b61b196fbda626ccdc22d032d8f
Cristiana David <cristiana.radu92@yahoo.com>
2025-04-08 12:57:35 +0100

    Add while loop for user input in instructions. Add reference board to show while game running and input current player spot choice.

--- run.py
+++ run.py
@@ -37,23 +37,35 @@ def show_instructions():
     print("by two players, in this case you and the computer, who alternately")
     print("place the marks X and O in one of the nine spaces in the grid.\n")
     print("Consider a board with the nine positions numbered as follows:\n")
-    print(" 0 | 1 | 2 |")
+    print(" 1 | 2 | 3 |")
     print("---|---|---|")
-    print(" 3 | 4 | 5 |")
+    print(" 4 | 5 | 6 |")
     print("---|---|---|")
-    print(" 6 | 7 | 8 |\n")
-    print("Choose a position from 0 to 8 in order to mark.")
+    print(" 7 | 8 | 9 |\n")
+    print("Choose a position from 1 to 9 in order to mark.")
     print("Whoever gets the first 3 symbols in a row, column or diagonal,")
     print("is the winner of the game!\n")
     print(">>>GOOD LUCK!<<<")
     print("Type 's' to start the game or 'e' to exit:")
+    while game_running:
+        play_choice = input().strip().lower()
+        if play_choice == 's':
+            clear_screen()
+            print_game_board(game_board)
+        elif play_choice == 'e':
+            clear_screen()
+            print("Thank you! If you change your mind,")
+            print("please press Run program!")
+            break
+        else:
+            print("Error! Please type in 's' or 'e'.")        
     
 
 def intro_user_input():
     """
     Welcomes the user and gives the option to continue or no.
     """
-    print("*" * 5 + "Welcome to TIC TAC TOE game" + "*" * 5 + "\n")
+    print("\n" + "*" * 5 + "Welcome to TIC TAC TOE game" + "*" * 5 + "\n")
     print("What is your name?")
     name = input()
     print("")
@@ -62,37 +74,53 @@ def intro_user_input():
     print("Would you like to play Tic Tac Toe?")
     print("Type 'y' for YES and 'n' for NO:")
     user_choice = input().strip().lower()
-    if user_choice == 'y':
-        clear_screen()
-        show_instructions()
-    elif user_choice == 'n':
-        clear_screen()
-        print("Thank you! If you change your mind, please press Run program!")
-    else:
-        print("Error! Please type 'y' or 'n'")    
+    while game_running:
+        if user_choice == 'y':
+            clear_screen()
+            show_instructions()
+        elif user_choice == 'n':
+            clear_screen()
+            print("Thank you! If you change your mind,")
+            print("please press Run program!")
+            break
+        else:
+            print("Error! Please type 'y' or 'n'")    
 
 
 def print_game_board(game_board):
     """
-    Creates the game board
+    Creates the game board, 
+    and prints reference board first for better UX.
     """
+    print("\nReference board:\n")
+    print("1 | 2 | 3 |")
+    print("--|---|---|")
+    print("4 | 5 | 6 |")
+    print("--|---|---|")
+    print("7 | 8 | 9 |\n")
+    print("Live play board:\n")
     print(game_board[0] + " | " + game_board[1] + " | " + game_board[2] + " |")
     print("--|---|---|")
     print(game_board[3] + " | " + game_board[4] + " | " + game_board[5] + " |")
     print("--|---|---|")
     print(game_board[6] + " | " + game_board[7] + " | " + game_board[8] + " |")
 
+    type_choice(game_board)
+
 
 def type_choice(game_board):
     """
-    Player to choose its spot from 0 to 8 to mark.
+    Player to choose its spot from 1 to 9 to mark.
     """
-    global current_player
-
-
-def validation():
-    pass 
-
+    inp = int(input("Choose a spot from 1 to 9: "))
+    if inp >= 1 and inp <= 9 and game_board[inp-1] == " ":
+        game_board[inp-1] = current_player
+    else:
+        raise ValueError("Spot already taken! Try again!")    
+    
+    clear_screen()
+    print_game_board(game_board)        
+      
 
 def switch_player():
     """
@@ -111,16 +139,12 @@ def computer(game_board):
     """
     while current_player == "O":
         position = random.randint(0, 8)
-        if game_board[position] == "":
+        if game_board[position] == " ":
             game_board[position] = "O"
             switch_player()
 
 
 def restart_game():
-    pass
-
-
-def exit_game(game_window):
     pass
             
 
@@ -128,12 +152,13 @@ def main():
     """
     Runs all the program functions.
     """
-    intro_user_input()
-    show_instructions()
-    print_game_board(game_board)
-    type_choice(game_board)
-    switch_player()
-    computer(game_board)
+    while game_running:
+        intro_user_input()
+        print_game_board(game_board)
+        type_choice(game_board)
+        switch_player()
+        computer(game_board)
+        switch_player()
 
 
 if __name__ == '__main__':
